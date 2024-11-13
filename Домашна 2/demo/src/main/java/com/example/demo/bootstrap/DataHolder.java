097aa9e147037527bb7b9c9302a9e6c28842154c
Marijana Petrovska <128865413+marijanamarijana@users.noreply.github.com>
2024-11-13 20:14:24 +0100

    Add files via upload

---
+++ Домашна 2/demo/src/main/java/com/example/demo/bootstrap/DataHolder.java
@@ -0,0 +1,14 @@
+package com.example.demo.bootstrap;
+
+import jakarta.annotation.PostConstruct;
+import org.hibernate.internal.util.collections.LinkedIdentityHashMap;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataHolder {
+
+    @PostConstruct
+    public void init(){
+
+    }
+}
