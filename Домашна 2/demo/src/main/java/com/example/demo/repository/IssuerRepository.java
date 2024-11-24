41fe0aaa4d19181190390fda15ea3e1b4cf05698
Marijana Petrovska <128865413+marijanamarijana@users.noreply.github.com>
2024-11-24 17:52:50 +0100

    Delete Домашна 2/demo directory

--- Домашна 2/demo/src/main/java/com/example/demo/repository/IssuerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Issuer;
-import com.example.demo.model.Issuer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssuerRepository extends JpaRepository<Issuer, Long> {
-
-}
--- Домашна 2/demo/src/main/java/com/example/demo/services/IssuerService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.services;
-
-public interface IssuerService {
-
-    // todo implement findAll() findById() ...
-}
