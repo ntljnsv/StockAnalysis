198cd85e69c4aa01de9ad1c483c2a59868ba719f
Ilina:) <137341909+IlinaDurakovska@users.noreply.github.com>
2024-12-18 20:08:11 +0100

    Delete Domashna3/backend_with_users/spring_backend directory

--- Domashna3/backend_with_users/spring_backend/src/main/java/com/makcii/spring_backend/model/exceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.makcii.spring_backend.model.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException() {
-        super("User already exists");
-    }
-}
