781b3c501bb7528cab538bee8a2ba63157066aee
ntljnsv <natalija.naseva9@gmail.com>
2024-12-21 15:36:15 +0100

    Added news sentiment table in db

--- Domashna3/db_init/init/init_tables.sql
+++ Domashna3/db_init/init/init_tables.sql
@@ -1,6 +1,18 @@
 CREATE TABLE IF NOT EXISTS issuers (
-    issuer_name varchar(50) primary key
+    issuer_name varchar(50) primary key,
+		current_recommendation varchar(10) default 'hold'
 );
+
+
+CREATE TABLE IF NOT EXISTS news_sentiment (
+    id serial primary key,
+    issuer_name varchar(50),
+    recommendation varchar(10), -- 'buy', 'sell', 'hold'
+    scraped_date date,
+		
+    CONSTRAINT fk_issuer_news FOREIGN KEY (issuer_name) REFERENCES issuers (issuer_name)
+);
+
 
 CREATE TABLE IF NOT EXISTS day_data (
     id serial primary key,
@@ -19,10 +31,12 @@ CREATE TABLE IF NOT EXISTS day_data (
 );
 
 
-DROP INDEX IF EXISTS idx_day_data_issuer_name;
-DROP INDEX IF EXISTS idx_day_data_date;
-DROP INDEX IF EXISTS idx_day_data_issuer_date;
+-- Day Data Table Indexes
+CREATE INDEX IF NOT EXISTS idx_day_data_issuer_name ON day_data (issuer_name);
+CREATE INDEX IF NOT EXISTS idx_day_data_date ON day_data ("date");
+CREATE INDEX IF NOT EXISTS idx_day_data_issuer_date ON day_data (issuer_name, "date");
 
-CREATE INDEX idx_day_data_issuer_name ON day_data (issuer_name);
-CREATE INDEX idx_day_data_date ON day_data ("date");
-CREATE INDEX idx_day_data_issuer_date ON day_data (issuer_name, "date");
+-- News Sentiment Table Indexes
+CREATE INDEX IF NOT EXISTS idx_news_sentiment_issuer_name ON news_sentiment (issuer_name);
+CREATE INDEX IF NOT EXISTS idx_news_sentiment_date ON news_sentiment (scraped_date);
+CREATE INDEX IF NOT EXISTS idx_news_sentiment_issuer_date ON news_sentiment (issuer_name, scraped_date);
