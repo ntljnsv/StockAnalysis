1135e7f1585f95d351344f13ad6c3932b8ab8967
ntljnsv <natalija.naseva9@gmail.com>
2024-12-22 04:34:25 +0100

    Completely changed approach to scraping for analysis

---
+++ Domashna3/sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,244 @@
+import re
+import fitz
+import torch
+import aiohttp
+import asyncio
+from io import BytesIO
+from datetime import datetime
+from aiohttp import ClientTimeout
+from lxml import etree
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+class DatabaseHelper:
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        self.engine = create_engine(self.connection_string)
+
+    def get_issuers(self):
+        query = "SELECT issuer_name FROM issuers"
+        with self.engine.connect() as conn:
+            result = conn.execute(text(query)).fetchall()
+        return [r[0] for r in result]
+
+    def get_latest_date(self, issuer):
+        query = "SELECT MAX(scraped_date) FROM news_sentiment WHERE issuer_name = :issuer"
+        with self.engine.connect() as conn:
+            result = conn.execute(text(query), {'issuer': issuer}).fetchone()
+        return result[0] if result else None
+
+    def save_data(self, data):
+        query = """
+            INSERT INTO news_sentiment (issuer_name, recommendation, scraped_date)
+            VALUES (:issuer_name, :recommendation, :scraped_date)
+        """
+
+        with self.engine.connect() as conn:
+            conn.execute(text(query), data)
+            conn.commit()
+
+    def update_current_recommendations(self):
+        query = """
+            WITH RecommendationCounts AS (
+                SELECT 
+                    issuer_name, 
+                    recommendation, 
+                    COUNT(*) AS count,
+                    RANK() OVER (PARTITION BY issuer_name ORDER BY COUNT(*) DESC) AS rank
+                FROM news_sentiment
+                WHERE scraped_date >= CURRENT_DATE - INTERVAL '60 days'
+                GROUP BY issuer_name, recommendation
+            )
+            UPDATE issuers
+            SET current_recommendation = rc.recommendation
+            FROM RecommendationCounts rc
+            WHERE issuers.issuer_name = rc.issuer_name AND rc.rank = 1;
+        """
+        with self.engine.connect() as conn:
+            conn.execute(text(query))
+            conn.commit()
+
+
+class Parser:
+
+    def clean_text(self, text):
+        text = ' '.join(text.split())
+        text = re.sub(r'\s+', ' ', text)
+        text = re.sub(r'[^\w\s]', '', text)
+        print(text)
+        return text.lower()
+
+    def parse_file(self, byte_code):
+        file = fitz.open(stream=byte_code)
+        text = ""
+        for page_num in range(min(2, file.page_count)):
+            page = file.load_page(page_num)
+            text += page.get_text("text")
+        text = self.clean_text(text)
+        print(text)
+        return text
+
+    def parse_html(self, content):
+        text = re.sub(r'<[^>]+>', '', content).strip()
+        text = self.clean_text(text)
+        return text
+
+
+class Scraper:
+
+    def __init__(self, session, parser):
+        self.session = session
+        self.link_url = "https://www.mse.mk/en/symbol/{issuer}"
+        self.text_url = "https://api.seinet.com.mk/public/documents/single/{news_id}"
+        self.attachment_url = "https://api.seinet.com.mk/public/documents/attachment/{attachment_id}"
+        self.parser = parser
+
+    def extract_date(self, link_text):
+        date_pattern = r'(\d{1,2}/\d{1,2}/\d{4})'
+        match = re.search(date_pattern, link_text)
+        if match:
+            date_string = match.group(1)
+            date = datetime.strptime(date_string, '%m/%d/%Y')
+            return date.date()
+
+    def extract_news_id(self, url):
+        return url.split('/')[-1]
+
+    async def get_issuer_news_links(self, issuer):
+        url = self.link_url.format(issuer=issuer)
+        async with self.session.get(url) as response:
+            response.raise_for_status()
+            data = await response.text()
+            tree = etree.HTML(data)
+            links = tree.xpath('//*[@id="seiNetIssuerLatestNews"]//a')
+            links = [
+                (self.extract_news_id(link.xpath('./@href')[0]),
+                 self.extract_date(link.xpath('./ul/li[2]/h4/text()')[0]))
+                for link in links
+                if link.xpath('./@href')
+            ]
+            return links
+
+    async def fetch_news(self, news_id):
+        news_url = self.text_url.format(news_id=news_id)
+        async with self.session.get(news_url) as response:
+            if response.status_code == 200:
+                data = response.json()
+                data = data.get("data", {}).get("content", None)
+                if data:
+                    return self.parser.parse_html(data)
+                data = data.get("data", {}).get("attachments", None)
+                if data:
+                    attachment_id = data[0].get("attachmentId", None)
+                    return self.fetch_attachment(attachment_id) if attachment_id else None
+        return None
+
+    async def fetch_attachment(self, attachment_id):
+        url = self.attachment_url.format(attachment_id=attachment_id)
+        async with self.session.get(url) as response:
+            if response.status_code == 200:
+                content = BytesIO(response.content)
+                return self.parser.parse_file(content)
+        return None
+
+
+class SentimentAnalyzer:
+
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+        self.model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
+
+    def get_action(self, label):
+        actions = {"positive": "buy", "negative": "sell", "neutral": "hold"}
+        return actions[label]
+
+    def get_label(self, prediction):
+        labels = self.model.config.id2label
+        return labels[prediction.item()]
+
+
+    def analyze_article(self, text):
+        inputs = self.tokenizer(text, max_length=512, truncation=True, padding='max_length', return_tensors='pt')
+        outputs = self.model(**inputs)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, axis=-1)
+        label = self.get_label(prediction)
+        return self.get_action(label)
+
+
+class Pipeline:
+
+    def __init__(self, db_connection_string):
+        self.db = DatabaseHelper(db_connection_string)
+        self.parser = Parser()
+        self.sentiment_analyzer = SentimentAnalyzer()
+
+    async def process_issuer(self, issuer, scraper):
+        latest_date = self.db.get_latest_date(issuer)
+        print(f"{issuer}: {latest_date}")
+        news_links = await scraper.get_issuer_news_links(issuer)
+        print(f"LINKS -> {issuer}: {news_links}")
+
+        filtered_links = [
+            (news_id, date) for news_id, date in news_links
+            if date > (latest_date or datetime.strptime('2014-01-01', '%Y-%m-%d').date())
+        ]
+
+        tasks = [
+            self.process_article(issuer, news_id, date, scraper) for news_id, date in filtered_links
+        ]
+
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        final_results = [result for result in results if result is not None]
+
+        print(f"DATA -> {issuer}: {final_results}")
+        return final_results
+
+    async def process_article(self, issuer, news_id, date, scraper):
+        content = scraper.fetch_news(news_id)
+        if content:
+            recommendation = self.sentiment_analyzer.analyze_article(content)
+            return issuer, recommendation, datetime.utcnow().strftime('%Y-%m-%d')
+
+    async def process_issuer_with_retry_with_semaphore(self, issuer, scraper, semaphore, retries=3, timeout=60):
+        attempt = 0
+        while attempt < retries:
+            try:
+                async with semaphore:
+                    results = await asyncio.wait_for(self.process_issuer(issuer, scraper), timeout)
+                return results
+            except asyncio.TimeoutError:
+                attempt += 1
+                print(f"Timeout exceeded while processing {issuer}. Attempt {attempt}/{retries}")
+                if attempt == retries:
+                    print(f"Max retries reached for {issuer}. Skipping this issuer.")
+                    break
+                await asyncio.sleep(2 ** attempt)
+            except asyncio.CancelledError:
+                print(f"Task for {issuer} was cancelled.")
+                break
+            except Exception as e:
+                print(f"An unexpected error occurred with {issuer}: {e}")
+                break
+
+    async def run(self):
+        issuers = self.db.get_issuers()
+        semaphore = asyncio.Semaphore(15)
+        async with aiohttp.ClientSession(timeout=ClientTimeout(total=120)) as session:
+            scraper = Scraper(session, self.parser)
+            tasks = [
+                self.process_issuer_with_retry_with_semaphore(issuer, scraper, semaphore)
+                for issuer in issuers
+            ]
+            results = await asyncio.gather(*tasks)
+            self.db.save_data(results)
+
+
+# Running the Pipeline
+if __name__ == "__main__":
+    db_connection_string = "postgresql+psycopg2://sa:p123@localhost:5432/Makcii_DB"
+    pipeline = Pipeline(db_connection_string)
+    asyncio.run(pipeline.run())
