ebf727f27182dd03c372c2c72dbcbe079575d8c5
ntljnsv <natalija.naseva9@gmail.com>
2025-01-04 23:27:00 +0100

    Deleted NoDataInPeriodException.java from spring_backend

--- Domashna4/spring_backend/src/main/java/com/makcii/spring_backend/model/exceptions/NoDataInPeriodException.java
+++
@@ -1,9 +0,0 @@
-package com.makcii.spring_backend.model.exceptions;
-
-import java.time.LocalDate;
-
-public class NoDataInPeriodException extends RuntimeException{
-    public NoDataInPeriodException(String issuer, LocalDate startDate, LocalDate endDate) {
-        super(String.format("No data available for %s from %s to %s", issuer, startDate, endDate));
-    }
-}
